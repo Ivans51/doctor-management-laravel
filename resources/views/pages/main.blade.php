c9fbd26d31f6ff5c9fdce22004f29b42280b7389
Ivans <ivans51.dev@gmail.com>
2023-04-13 17:15:12 -0400

    refactor(front): change css class

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -6,8 +6,8 @@
 
     <section id="head-info" class="text-white my-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
 
-        <div class="p-6 rounded-lg flex items-center" style="background: #8364fe">
-            <div class="p-4 bg-white bg-opacity-50" style="border-radius: 50%">
+        <div class="box-context" style="background: #8364fe">
+            <div class="box-left">
                 <x-ri-calendar-line/>
             </div>
             <div class="ml-4">
@@ -16,8 +16,8 @@
             </div>
         </div>
 
-        <div class="p-6 rounded-lg flex items-center" style="background: #f75166">
-            <div class="p-4 bg-white bg-opacity-50" style="border-radius: 50%">
+        <div class="box-context" style="background: #f75166">
+            <div class="box-left">
                 <x-ri-user-3-line/>
             </div>
             <div class="ml-4">
@@ -26,8 +26,8 @@
             </div>
         </div>
 
-        <div class="p-6 rounded-lg flex items-center" style="background: #f8ab16">
-            <div class="p-4 bg-white bg-opacity-50" style="border-radius: 50%">
+        <div class="box-context" style="background: #f8ab16">
+            <div class="box-left">
                 <x-ri-video-line/>
             </div>
             <div class="ml-4">
@@ -36,8 +36,8 @@
             </div>
         </div>
 
-        <div class="p-6 rounded-lg flex items-center" style="background: #4ca4f9">
-            <div class="p-4 bg-white bg-opacity-50" style="border-radius: 50%">
+        <div class="box-context" style="background: #4ca4f9">
+            <div class="box-left">
                 <x-ri-video-chat-line/>
             </div>
             <div class="ml-4">
