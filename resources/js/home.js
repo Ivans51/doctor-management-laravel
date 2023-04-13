1eb32e4e88a4be005e1897a2d5a38082ef402a8b
Ivans <ivans51.dev@gmail.com>
2023-04-13 16:13:21 -0400

    feat(front): add bottom menu in header

---
+++ resources/js/home.js
@@ -0,0 +1,17 @@
+function openCloseHeaderBtn(content, btn) {
+    const contentMenu = document.getElementById(content)
+    const btnMenu = document.getElementById(btn)
+    document.addEventListener('click', (event) => {
+        const isBtnCurrency = btnMenu.contains(event.target)
+        const isContentCurrency = contentMenu.contains(event.target)
+
+        if (isBtnCurrency && (contentMenu.style.display === 'none' || contentMenu.style.display === '')) {
+            contentMenu.style.display = 'block'
+        } else if (!isContentCurrency) {
+            contentMenu.style.display = 'none'
+        }
+    })
+}
+
+openCloseHeaderBtn('content-notification', 'btn-notification')
+openCloseHeaderBtn('content-user', 'btn-user')
