a5d77b4af5a37b5100c6e36869a3e9c136349f43
Ivans <ivans51.dev@gmail.com>
2023-04-22 11:38:18 -0400

    feat: add payments view

--- resources/views/pages/payments.blade.php
+++ resources/views/pages/payments.blade.php
@@ -1,19 +1,39 @@
 @extends('home')
 
 @section('content')
-    <h1 class="font-bold text-2xl mb-1">Welcome, Dr. Stephen</h1>
-    <p class="text-sm">Have a nice day at great work</p>
-
     <section class="mt-10">
-        <h3 class="my-2 font-bold text-lg">Recent Patients</h3>
+        <h3 class="my-2 font-bold text-lg">Payment List</h3>
+
+        <div class="my-4 flex flex-col lg:flex-row space-x-0 lg:space-x-4 space-y-2 lg:space-y-0 w-full">
+            <label>Show
+                <select class="bg-white p-1 border ml-2" name="show" id="show">
+                    <option value="10">10</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+            </label>
+        </div>
+
+        <div class="my-4 flex flex-col lg:flex-row space-x-0 lg:space-x-4 space-y-2 lg:space-y-0 w-full">
+            <label class="w-full lg:w-2/3">
+                Search
+                <input class="w-full" type="search" placeholder="Patient name, status, type">
+            </label>
+            <label class="w-full lg:w-1/3">
+                Date
+                <input class="w-full" type="date">
+            </label>
+        </div>
+
         <table class="w-full table-auto" style="border-collapse: separate; border-spacing: 0 8px;">
             <tr class="bg-zinc-100">
                 <th class="px-4 py-1 text-left">Patient Name</th>
-                <th class="px-4 py-1">Visit Id</th>
+                <th class="px-4 py-1">Amount</th>
                 <th class="px-4 py-1">Date</th>
-                <th class="px-4 py-1">Gender</th>
-                <th class="px-4 py-1">Diseases</th>
+                <th class="px-4 py-1">Type</th>
+                <th class="px-4 py-1">ID</th>
                 <th class="px-4 py-1">Status</th>
+                <th class="px-4 py-1">Action</th>
             </tr>
             @foreach($images as $image)
                 <tr class="bg-white rounded">
@@ -34,8 +54,11 @@
                         1
                     </td>
                     <td class="px-4 py-2 text-center">
+                        1
+                    </td>
+                    <td class="px-4 py-2 text-center">
                         <button class="rounded text-red-900 bg-red-100 px-4 py-1 text-sm ml-2">
-                            Options
+                            Details
                         </button>
                     </td>
                 </tr>
