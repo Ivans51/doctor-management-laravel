9bfaa06c80e428b74854843a3713a7d8886325e7
Ivans <ivans51.dev@gmail.com>
2023-05-09 16:55:16 -0400

    ci: add resources js in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/home.js',
+                'resources/js/modal.js',
+                'resources/js/phone-input.js',
+                'resources/js/settings-menu.js',
+            ],
             refresh: true,
         }),
     ],
